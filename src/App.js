808960caf14cae2bd2dc646948cf0fc641b56fbc
RenatoMGarcia <contatormg1@gmail.com>
2021-08-24 17:37:23 -0300

    Teste

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from "react"
 import ListaDeNotas from "./components/ListaDeNotas";
 import FormularioCadastro from "./components/FormularioCadastro";
 
+
 class App extends Component{
   render() {
     return (
-      <section>
+      <div>
         <FormularioCadastro />
         <ListaDeNotas />
-      </section>
+      </div>
     );
   }
 }
